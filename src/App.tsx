00d3443a92c25457eaac9c360292c3b76fbabeec
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-30 11:05:48 +0200

    Calculator part is abstracted into a seperate component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,10 @@
+import Calculator from "./components/Calculator";
+
 function App() {
   return (
     <div className="bg-gray-900">
       <div className=" mx-auto bg-[url(public/yuhos.png)] h-screen w-screen max-w-[1350px] bg-no-repeat bg-cover flex justify-center items-center">
-        <div className="w-[75vw] md:w-[50vw] grid justify-center gap-4 p-[3rem] rounded-md shadow-md shadow-gray-950 bg-gray-950/[0.7]">
-          <h1 className="text-[2rem] text-center mb-[2rem] text-slate-200 font-semibold">
-            Delivery Fee Calculator
-          </h1>
-          <form className="grid gap-6" action="">
-            <div className="grid gap-2">
-              <label
-                className="text-slate-200 text-[1.2rem]"
-                htmlFor="cartValue"
-              >
-                Cart value (€)
-              </label>
-              <input
-                className="p-[0.5rem] bg-slate-300 text-slate-950 border border-1 rounded-md border-blue-500 focus:border-lime-500 focus:outline-none"
-                type="text"
-                name="cartValue"
-                id="cartValue"
-              />
-            </div>
-            <div className="grid gap-2">
-              <label
-                className="text-slate-200 text-[1.2rem]"
-                htmlFor="distance"
-              >
-                Delivery distance (m)
-              </label>
-              <input
-                className="p-[0.5rem] bg-slate-300 text-slate-800 border border-1 rounded-md border-blue-500 focus:border-lime-500 focus:outline-none"
-                type="text"
-                name="distance"
-                id="distance"
-              />
-            </div>
-            <div className="grid gap-2">
-              <label
-                className="text-slate-200 text-[1.2rem]"
-                htmlFor="itemNumber"
-              >
-                Amount of items
-              </label>
-              <input
-                className="p-[0.5rem] bg-slate-300 text-slate-800 border border-1 rounded-md border-blue-500 focus:border-lime-500 focus:outline-none"
-                type="text"
-                name="itemNumber"
-                id="itemNumber"
-              />
-            </div>
-            <div className="grid gap-2">
-              <label
-                className="text-slate-200 text-[1.2rem]"
-                htmlFor="dateTime"
-              >
-                Time
-              </label>
-              <input
-                className="p-[0.5rem] bg-slate-300 text-slate-800 border border-1 rounded-md border-blue-500 focus:border-lime-500 focus:outline-none"
-                type="datetime-local"
-                name="dateTime"
-                id="dateTime"
-              />
-            </div>
-            <button
-              className="border mt-[1rem] py-[0.5rem] rounded-md"
-              type="submit"
-            >
-              Calculate delivery price
-            </button>
-          </form>
-          <p>Delivery price is {}€</p>
-        </div>
+        <Calculator />
       </div>
     </div>
   );
