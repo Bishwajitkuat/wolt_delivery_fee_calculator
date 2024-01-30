3bb6d99359b139c363fd034cd951ca62579af85b
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-30 20:03:20 +0200

    min width added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <div className="bg-gray-900">
-      <div className=" mx-auto bg-[url(public/yuhos.png)] h-screen w-screen max-w-[1350px] bg-no-repeat bg-cover flex justify-center items-center">
-        <Calculator />
-      </div>
+    <div className=" mx-auto bg-[url(/yuhos.png)] h-screen w-screen min-w-[350px] bg-no-repeat bg-cover flex justify-center items-center">
+      <Calculator />
     </div>
   );
 }
