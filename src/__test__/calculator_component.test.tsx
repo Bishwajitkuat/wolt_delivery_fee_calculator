ce328c1ceea9733fb8e4970265efe4ec12df31c6
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-01-31 03:10:40 +0200

    Test cases added to test whether input fields register changes.

--- src/__test__/calculator_component.test.tsx
+++ src/__test__/calculator_component.test.tsx
@@ -1,16 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Calculator from "../components/Calculator";
 
 // Testing components
 
-describe("Testing components", () => {
+describe("Testing Calculator", () => {
   it("Is Calculator component present in DOM", () => {
     render(<Calculator />);
     const CalculatorComponent = screen.getByTestId("calculator");
     expect(CalculatorComponent).toBeDefined();
   });
-  it("Is content of delivery fee span empty at begining", () => {
+  it("Testing cartValue input field register change", () => {
     render(<Calculator />);
-    expect(screen.getByTestId("deliveryFee").innerHTML).toBe("");
+    const cartValueInputElement: HTMLInputElement =
+      screen.getByPlaceholderText(/example: 15/i);
+    fireEvent.change(cartValueInputElement, { target: { value: 20 } });
+    expect(cartValueInputElement.value).toEqual("20");
+  });
+  it("Testing distance input field register change", () => {
+    render(<Calculator />);
+    const cartValueInputElement: HTMLInputElement =
+      screen.getByPlaceholderText(/example: 1200/i);
+    fireEvent.change(cartValueInputElement, { target: { value: 1200 } });
+    expect(cartValueInputElement.value).toEqual("1200");
+  });
+  it("Testing itemNumber input field register change", () => {
+    render(<Calculator />);
+    const cartValueInputElement: HTMLInputElement =
+      screen.getByPlaceholderText(/example: 4/i);
+    fireEvent.change(cartValueInputElement, { target: { value: 8 } });
+    expect(cartValueInputElement.value).toEqual("8");
+  });
+  it("Testing dateTime input field register change", () => {
+    render(<Calculator />);
+    const cartValueInputElement: HTMLInputElement =
+      screen.getByPlaceholderText(/dateTime/i);
+    fireEvent.change(cartValueInputElement, {
+      target: { value: "2023-06-14T00:00" },
+    });
+    expect(cartValueInputElement.value).toEqual("2023-06-14T00:00");
   });
 });
